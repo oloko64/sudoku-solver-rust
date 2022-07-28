f9068a9fced59fb663d53c4fde2852950cf86dbb
OLoKo64 <naldoepingo1@gmail.com>
2022-07-28 00:50:44 -0300

    feat: first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sudoku solver in Rust
+
+Code written to solve a sudoku puzzle.
+
+The puzzle has to be of size 9x9.
---
+++ src/main.rs
@@ -0,0 +1,104 @@
+use std::time::Instant;
+
+enum BoardSpecs {
+    GridSize = 9,
+    GridBlock = 3,
+}
+
+fn main() {
+    let mut board: [[u8; 9]; 9] = [
+        [7, 0, 2, 0, 5, 0, 6, 0, 0],
+        [0, 0, 0, 0, 0, 3, 0, 0, 0],
+        [1, 0, 0, 0, 0, 9, 5, 0, 0],
+        [8, 0, 0, 0, 0, 0, 0, 9, 0],
+        [0, 4, 3, 0, 0, 0, 7, 5, 0],
+        [0, 9, 0, 0, 0, 0, 0, 0, 8],
+        [0, 0, 9, 7, 0, 0, 0, 0, 5],
+        [0, 0, 0, 2, 0, 0, 0, 0, 0],
+        [0, 0, 7, 0, 4, 0, 2, 0, 3],
+    ];
+
+    let time = Instant::now();
+
+    println!("\n    Original board:\n");
+    print_board(&board);
+
+    if solve_board(&mut board) {
+        println!("\n    Solved board:\n");
+        print_board(&board);
+    } else {
+        println!("\n  No solution found.");
+    }
+
+    println!("\nTime taken: {}ms", time.elapsed().as_millis());
+}
+
+fn is_number_in_row(board: &mut [[u8; 9]; 9], number: u8, row: u8) -> bool {
+    for i in 0..BoardSpecs::GridSize as u8 {
+        if board[row as usize][i as usize] == number {
+            return true;
+        }
+    }
+    false
+}
+
+fn is_number_in_column(board: &mut [[u8; 9]; 9], number: u8, column: u8) -> bool {
+    for i in 0..BoardSpecs::GridSize as u8 {
+        if board[i as usize][column as usize] == number {
+            return true;
+        }
+    }
+    false
+}
+
+fn is_number_in_box(board: &mut [[u8; 9]; 9], number: u8, box_row: u8, box_column: u8) -> bool {
+    for i in 0..BoardSpecs::GridBlock as u8 {
+        for j in 0..BoardSpecs::GridBlock as u8 {
+            if board[(box_row + i) as usize][(box_column + j) as usize] == number {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn is_valid_place(board: &mut [[u8; 9]; 9], number: u8, row: u8, column: u8) -> bool {
+    !is_number_in_row(board, number, row)
+        && !is_number_in_column(board, number, column)
+        && !is_number_in_box(board, number, row - row % BoardSpecs::GridBlock as u8, column - column % BoardSpecs::GridBlock as u8)
+}
+
+fn solve_board(board: &mut [[u8; 9]; 9]) -> bool {
+    for i in 0..BoardSpecs::GridSize as u8 {
+        for j in 0..BoardSpecs::GridSize as u8 {
+            if board[i as usize][j as usize] == 0 {
+                for k in 1..=BoardSpecs::GridSize as u8 {
+                    if is_valid_place(board, k, i, j) {
+                        board[i as usize][j as usize] = k;
+                        if solve_board(board) {
+                            return true;
+                        }
+                        board[i as usize][j as usize] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn print_board(board: &[[u8; 9]; 9]) {
+    for i in 0..BoardSpecs::GridSize as u8 {
+        if i % BoardSpecs::GridBlock as u8 == 0 && i != 0 {
+            println!("-----------------------");
+        }
+        for j in 0..BoardSpecs::GridSize as u8 {
+            if j % BoardSpecs::GridBlock as u8 == 0 && j != 0 {
+                print!(" | ");
+            }
+            print!("{} ", board[i as usize][j as usize]);
+        }
+        println!();
+    }
+}
